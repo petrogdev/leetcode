a6b89fff5c757d25c0b29262b5e5a56414491c6a
Petro <petro.gorobchenko@ayapayments.com>
2020-06-26 00:24:28 -0400

    Updating with more examples

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/petrogdev/leetcode
+
+go 1.13
--- solutions/remove_duplicates/remove_duplicates.go
+++ solutions/remove_duplicates/remove_duplicates.go
@@ -1,1 +1,11 @@
 package remove_duplicates
+
+func removeDuplicates(nums []int) int {
+	for index := 1 ;index < len(nums); index ++ {
+		if nums[index-1] == nums[index]  {
+			nums = append(nums[:index], nums[index+1:]...)
+			index --
+		}
+	}
+	return len(nums)
+}
